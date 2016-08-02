fcaf8a6fe36ad635fcee868b538332a835ac475e
cwen-coder <172569856@qq.com>
2016-08-02 01:27:25 +0000

    添加config 结构

--- lib/config.go
+++ lib/config.go
@@ -1,4 +1,19 @@
 package lib
 
 type SiteConfig struct {
+	Title     string
+	Introduce string
+	Limit     int
+	Theme     string
+	URL       string
+}
+
+type AuthorConfig struct {
+	Name string
+}
+
+type BuildConfig struct {
+	Port    string
+	Copy    []string
+	Publish string
 }
