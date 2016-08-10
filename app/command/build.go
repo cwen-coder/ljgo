165e04094e21ed846c0766519d9ede4e15fc0697
cwen-coder <172569856@qq.com>
2016-08-10 23:42:21 +0800

    添加buildTpl

--- app/command/build.go
+++ app/command/build.go
@@ -2,10 +2,12 @@ package command
 
 import (
 	"fmt"
+	"html/template"
 	"io/ioutil"
 	"os"
 	"os/signal"
 	"path/filepath"
+	"strings"
 	"syscall"
 
 	"github.com/qiniu/log"
@@ -37,14 +39,42 @@ func runBuild(c *cli.Context) error {
 func build() {
 	themePath := filepath.Join(rootPath, globalConfig.Site.Theme)
 	partialPath := filepath.Join(themePath, "Tpl")
-	files, err := filepath.Glob(filepath.Join(partialPath, "*.tpl"))
+	partialTpl := buildPartialTpl(partialPath)
+
+	articleTpl := buildTpl(filepath.Join(themePath, "article.html"), partialTpl, "article")
+	log.Info(articleTpl)
+}
+
+func buildPartialTpl(path string) string {
+	files, err := filepath.Glob(filepath.Join(path, "*.tpl"))
 	if err != nil {
 		log.Fatal(err)
 	}
+	var partialTpl string
 	for _, path := range files {
-		_, err := ioutil.ReadFile(path)
+		html, err := ioutil.ReadFile(path)
 		if err != nil {
 			log.Fatal(err)
 		}
+		fileName := filepath.Base(path)
+		fileName = strings.TrimSuffix(strings.TrimPrefix(fileName, "T."), ".tpl")
+		htmlStr := "{define \"" + fileName + " \"}" + string(html) + "{end}"
+		partialTpl += htmlStr
 	}
+	return partialTpl
+}
+
+func buildTpl(path string, partialTpl string, name string) template.Template {
+	html, err := ioutil.ReadFile(path)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	htmlStr := string(html) + partialTpl
+
+	tpl, err := template.New(name).Parse(htmlStr)
+	if err != nil {
+		log.Fatal(err)
+	}
+	return *tpl
 }
